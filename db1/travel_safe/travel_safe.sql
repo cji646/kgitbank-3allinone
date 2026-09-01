da512afe388df8dadbb8f2bd72375b1a1c5ea2f1
yujin-0223 <hvrn3803@gmail.com>
2026-09-01 17:51:48 +0900

    Update database dump without user data

--- db1/travel_safe/travel_safe.sql
+++ db1/travel_safe/travel_safe.sql
@@ -52,6 +52,26 @@ LOCK TABLES `countries` WRITE;
 INSERT INTO `countries` VALUES (1,'대한민국','아시아','안전','112 / 119','여행 중 소지품 관리에 주의하고, 응급 상황 발생 시 긴급전화 112 또는 119로 신고하세요.','2026-09-01 15:33:24'),(2,'대한민국','동북아시아','1단계 (여행유의)','경찰 112 / 소방·구급 119','집회·시위 지역 접근 자제, 소매치기 및 절도 주의','2026-09-01 15:48:11'),(3,'일본','동북아시아','1단계 (여행유의)','경찰 110 / 소방·구급 119','지진·쓰나미 등 자연재해 주의, 태풍·폭우 시 기상정보 확인','2026-09-01 15:48:11'),(4,'베트남','동남아시아','1단계 (여행유의)','경찰 113 / 소방 114 / 구급 115','오토바이 교통사고 주의, 소매치기 및 절도 주의','2026-09-01 15:48:11');
 /*!40000 ALTER TABLE `countries` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `user_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `password` varchar(255) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `role` enum('user','admin') NOT NULL DEFAULT 'user',
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -62,4 +82,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-09-01 17:44:12
+-- Dump completed on 2026-09-01 17:50:01
